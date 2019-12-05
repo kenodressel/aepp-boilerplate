b6ac4e4ddfce648f0431385756770a7114a61443
keno dressel <aeternity@keno.digital>
2019-12-05 10:50:07 +0100

    adds tests to verify tailwind and components are working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,25 @@
     {{address}}
     <h1>Balance</h1>
     {{balance}} AE
+    <br>
+    <br>
+    <br>
+    <h2>Components Test</h2>
+    <span class="hidden">If you see this, tailwindcss is not working</span>
+    <div class="mt-2 ml-2">
+      <ae-button face="round" fill="primary"> Primary Button </ae-button>
+    </div>
   </div>
 </template>
 
 <script>
-  import aeternity from "../utils/aeternity";
-  import axios from "axios";
+  import aeternity from '../utils/aeternity'
+  import axios from 'axios'
+  import {AeButton} from '@aeternity/aepp-components/src/components'
 
   export default {
     name: 'Home',
-    components: {},
+    components: { AeButton },
     data() {
       return {
         address: null,
